a8b6415bc6a15b368ec502cd5f68155566d7e719
Marion Balba <balba.marion@gmail.com>
2025-06-10 22:56:37 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Task from '@/components/Task.vue'
+</script>
+
+<template>
+  <div class="max-w-3xl mx-auto px-4 py-2 my-6 flex gap-4 justify-center">
+    <input type="text" class="rounded border-1 p-1">
+    <button class="rounded bg-green-400 p-2">
+      <i class="pi pi-plus"></i>
+      Add Task
+    </button>
+  </div>
+
+  <div class="max-w-3xl mx-auto px-4 py-2">
+    <Task name="Hello" />
+  </div>
+</template>
---
+++ src/components/Task.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps({
+    name: String,
+})
+</script>
+
+<template>
+    <div class="rounded border-1 border-gray-500">
+      <div class="flex gap-2 p-2">
+        <input class="mt-2" type="checkbox">
+        <div class="">
+          <p>Task Title</p>
+          <p class="italic text-gray-500">Task description</p>
+        </div>
+      </div>
+    </div>
+</template>
